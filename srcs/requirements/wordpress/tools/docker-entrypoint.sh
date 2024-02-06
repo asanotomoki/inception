a474756cb6a7226f1a631f39df639e4da54b3397
Tomoki Asano <mirai20190820@gmail.com>
2024-02-06 16:58:55 +0900

    config に環境変数の適応

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -7,14 +7,18 @@ mv wp-cli.phar /usr/local/bin/wp
 
 if [ ! -f /var/www/html/wp-config.php ]; then
     cp /tmp/wp-config.php /var/www/html/wp-config.php
+    sed -i "s/WP_DB_NAME/$DB_NAME/g" /var/www/html/wp-config.php
+    sed -i "s/WP_DB_USER/$DB_USER/g" /var/www/html/wp-config.php
+    sed -i "s/WP_DB_PASSWORD/$DB_PASSWORD/g" /var/www/html/wp-config.php
+
 fi
 if [ ! -f /var/www/html/wp-admin ]; then
     wp core download --allow-root --path=/var/www/html
 fi
 
+wp core install --allow-root --path=/var/www/html --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
 
-wp core install --allow-root --path=/var/www/html --url=localhost --title=tasano --admin_user=wordpress --admin_password=wordpress --admin_email=email@example.com
-
+echo $DOMAIN_NAME $WP_TITLE $ADMIN_USER $ADMIN_PASSWORD $ADMIN_EMAIL
 mv /tmp/www.conf /etc/php/7.4/fpm/pool.d/www.conf
 
 php-fpm7.4 -F
--- srcs/requirements/wordpress/tools/wp-config.php
+++ srcs/requirements/wordpress/tools/wp-config.php
@@ -1,16 +1,16 @@
 <?php
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', $_ENV['WORDPRESS_DB_NAME']);
+define('DB_NAME', 'WP_DB_NAME');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'WP_DB_USER');
 
 /** MySQL database password */
-define('DB_PASSWORD', '123456');
+define('DB_PASSWORD', 'WP_DB_PASSWORD');
 
 /** MySQL hostname */
-define('DB_HOST', $_ENV['WORDPRESS_DB_HOST']);
+define('DB_HOST', 'db:3306');
 
 /** Database Charset to use in creating database tables.*/
 define('DB_CHARSET', 'utf8');
