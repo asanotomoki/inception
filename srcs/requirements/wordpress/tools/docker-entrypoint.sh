2384a83e3351ee28b242cad630530bb2c64b7cdb
Tomoki Asano <mirai20190820@gmail.com>
2024-02-09 18:09:13 +0900

    usernotuika

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -3,6 +3,10 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+if echo "$ADMIN_USER" | grep -q -i "admin"; then
+    echo "Error: The admin user name cannot include 'admin'."
+    exit 1
+fi
 
 if [ ! -f /var/www/html/wp-config.php ]; then
     cp /tmp/wp-config.php /var/www/html/wp-config.php
@@ -10,13 +14,13 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     sed -i "s/WP_DB_USER/$DB_USER/g" /var/www/html/wp-config.php
     sed -i "s/WP_DB_PASSWORD/$DB_PASSWORD/g" /var/www/html/wp-config.php
 fi
-if [ ! -f /var/www/html/wp-admin ]; then
+
+if [ ! -d /var/www/html/wp-admin ]; then
     wp core download --allow-root --path=/var/www/html
 fi
 
-wp core install --allow-root --path=/var/www/html --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
+wp core install --allow-root --path=/var/www/html --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL"
+wp user create --allow-root --path=/var/www/html "$USER_NAME" "$USER_EMAIL" --role=author --user_pass="$USER_PASSWORD"
 
-echo $DOMAIN_NAME $WP_TITLE $ADMIN_USER $ADMIN_PASSWORD $ADMIN_EMAIL
 mv /tmp/www.conf /etc/php/7.4/fpm/pool.d/www.conf
-
 php-fpm7.4 -F
